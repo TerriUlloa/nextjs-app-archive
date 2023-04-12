385f1a4177738e58f36010fe302bac76d5aaba8b
Terri Ulloa <v-terriulloa@microsoft.com>
2023-04-12 16:32:27 -0700

    test commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    headers: async () => {
+        return [
+            {
+                source: '/:slug*',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload'
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block'
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN'
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin'
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+                    }
+                ]
+            }
+        ]
+    }
 }
 
 module.exports = nextConfig
